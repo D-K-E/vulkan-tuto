0871c5aed0237199378e2ec58e66ba747769d658
Qm Auber <qm.auber@tutanota.com>
2020-11-28 17:18:56 +0100

    base window done

---
+++ include/external.hpp
@@ -0,0 +1,12 @@
+#pragma once
+// external libraries
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+//
+
+//
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <string>
---
+++ src/ex01.cpp
@@ -0,0 +1,111 @@
+// main file
+#include <GLFW/glfw3.h>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
+#include <external.hpp>
+#include <stdexcept>
+//
+const uint32_t WIDTH = 800;
+const uint32_t HEIGHT = 600;
+
+/**
+  @brief Hello Triangle application object.
+
+  The application contains most of the steps for dealing
+  with
+  vulkan applications.
+ */
+class HelloTriangle {
+public:
+  std::string win_title = "Vulkan Window";
+  uint32_t win_width = WIDTH;
+  uint32_t win_height = HEIGHT;
+
+public:
+  HelloTriangle() {}
+  HelloTriangle(std::string wTitle, const uint32_t &w,
+                const uint32_t &h)
+      : win_title(wTitle), win_width(w), win_height(h) {}
+  /**
+    Run application.
+
+    Steps to run the application
+   */
+  void run() {
+    // 1. launch window
+    initWindow();
+
+    // 2. launch vulkan
+    initVulkan();
+
+    // 3. main loop
+    renderLoop();
+
+    // 4. clean up ressources
+    cleanUp();
+  }
+
+private:
+  GLFWwindow *window; // window for visualizing object.
+
+  /**
+    Initialize window.
+
+    Gives window hints. Sets its size, its title, etc.
+   */
+  void initWindow() {
+    if (glfwInit() == 0) {
+      throw std::runtime_error(
+          "Unable to launch glfw window");
+    }
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    //
+    window = glfwCreateWindow(win_width, win_height,
+                              win_title.c_str(), nullptr,
+                              nullptr);
+  }
+
+  /**
+    Initialize vulkan
+   */
+  void initVulkan() {}
+
+  /**
+    Rendering loop.
+
+    Render elements to window. Acquire user input
+   */
+  void renderLoop() {
+    //
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
+    }
+  }
+
+  /**
+    Clean up ressources.
+
+    Destroy window, and other ressources.
+   */
+  void cleanUp() {
+    //
+    glfwDestroyWindow(window);
+    glfwTerminate();
+  }
+};
+
+int main() {
+  std::string wtitle = "Vulkan Window Title";
+  HelloTriangle hello(wtitle, (uint32_t)640, (uint32_t)480);
+
+  try {
+    hello.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
+  return EXIT_SUCCESS;
+}
